906ae07764f5f27a45e23b78ce08612ae79f89c6
tomisin2k <tomithescorpio@gmail.com>
2025-06-24 00:06:08 +0100

    another change made

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import Navbar from './components/navbar';
+import Hero from './components/Hero';
+// import './components/hero.css'; 
 import Socialproof from './components/socialproof';
 import Testimonial from './components/testimonial';
 import Faq from './components/faq';
