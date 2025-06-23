e15f6b082bcd63ab077c9c9e744fa4e32a1aa668
tomisin2k <tomithescorpio@gmail.com>
2025-06-24 00:28:58 +0100

    another change made

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import './components/hero.css';
+import './components/hero.css'; // Leave this line in
+import Socialproof from './components/Socialproof';
+import Testimonial from './components/Testimonial';
+import Faq from './components/Faq';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
       <Navbar />
       <Hero />
+      <Socialproof />
+      <Testimonial />
+      <Faq />
+      <Footer />
     </>
   );
 }
