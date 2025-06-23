6d545958b5924990c0cae0a1e19fff85f8948993
tomisin2k <tomithescorpio@gmail.com>
2025-06-23 07:05:21 +0100

    fix: corrected import paths to match case-sensitive file system

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Navbar from './components/navbar';
-import './hero.css';
+import './components/hero.css'; 
 import Socialproof from './components/socialproof';
 import Testimonial from './components/testimonial';
 import Faq from './components/faq';
