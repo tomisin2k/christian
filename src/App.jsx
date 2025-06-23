d42c076e7ffc36995c712d8959dc85532f9e6297
tomisin2k <tomithescorpio@gmail.com>
2025-06-24 00:19:27 +0100

    another change made

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,13 @@
 import React from 'react';
-// import Navbar from './components/Navbar';
+import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import './components/hero.css';
-// import Socialproof from './components/Socialproof';
-// import Testimonial from './components/Testimonial';
-// import Faq from './components/Faq';
-// import Footer from './components/Footer';
 
 function App() {
   return (
     <>
-      {/* <Navbar /> */}
+      <Navbar />
       <Hero />
-      {/* <Socialproof />
-      <Testimonial />
-      <Faq />
-      <Footer /> */}
     </>
   );
 }
